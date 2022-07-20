43109e881484d487c95fdc23f77ad9cb524b5f4e
RalleeDev <rasmus.edelholt@outlook.com>
2022-07-20 23:22:42 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print(f"     Muscial Waffle      \n         [1] Password Generator      [2] Settings\n")
+choice = input("Choice: ")
+
+if choice == "1":
+    print("Option " + choice)
+elif choice == "2":
+    print("Option" + choice)
